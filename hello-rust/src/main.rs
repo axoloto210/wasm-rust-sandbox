fb269df5e197e2f8aef9b4de5eaccc30913f3e11
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-04-06 07:55:48 +0900

    borrowing

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, WASM!");
+    let hello_world = new_hello_world(0);
+    say(&hello_world);
+    say(&hello_world);
+
+}
+
+struct HelloWorld {
+    counter: u32,
+}
+
+fn say(hello_world: &HelloWorld){
+    println!("カウンター値は{}",hello_world.counter);
+}
+
+fn new_hello_world (counter: u32) -> HelloWorld {
+    HelloWorld { counter }
 }
