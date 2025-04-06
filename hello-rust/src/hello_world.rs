e959ea121695517dcec35975576bb0221271018f
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-04-06 12:04:10 +0900

    ResultとOption

--- hello-rust/src/hello_world.rs
+++ hello-rust/src/hello_world.rs
@@ -1,6 +1,9 @@
+use std::{num::ParseIntError, str::FromStr};
+
 #[derive(Debug)]
 pub struct HelloWorld {
     counter: u32,
+    message: String,
 }
 
 impl Into<u32> for HelloWorld {
@@ -15,9 +18,30 @@ impl Into<u64> for HelloWorld {
     }
 }
 
+impl  HelloWorld {
+    pub fn serialize(self)->String{
+        format!("{}\t{}", self.counter, self.message)
+    }    
+
+    pub fn deserialize(input:String)->Result<HelloWorld, ParseIntError>{
+        let find_result = input.find('\t');
+        let index = find_result.unwrap_or(0);
+        let (first, second) = input.split_at(index);
+
+        let counter = u32::from_str(first)?;
+
+        let message = if index > 0 {
+            second.trim_start().to_string()
+        }else {
+            second.to_string()
+        };
+        Ok(HelloWorld::new(counter, message))
+    }
+}
+
 impl HelloWorld {
-    pub fn new(counter: u32) -> Self {
-        HelloWorld { counter }
+    pub fn new(counter: u32, message: String) -> Self {
+        HelloWorld { counter, message }
     }
 
     // 所有権が移動
@@ -26,7 +50,7 @@ impl HelloWorld {
     }
 
     pub fn say(&self) {
-        println!("カウンター値は{}", self.counter);
+        println!("{}({}回目)", self.message, self.counter);
     }
     pub fn countup(&mut self) {
         self.counter += 1;
