a12f2ebf72fe59403dbba3bda5c90bd9fee31dc1
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-04-09 08:29:20 +0900

    bindgenマクロによるラッパーオブジェクトを使用

--- greet-user/src/main.rs
+++ greet-user/src/main.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
 
-use wasmtime::component::{Component, Linker, TypedFunc};
 use wasmtime::{Engine, Store};
+use wasmtime::component::{bindgen, Component, Linker, TypedFunc};
+
+bindgen!({
+    world:"greetable-provider",
+    path: "../greet/wit"
+});
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -24,38 +29,51 @@ fn start(args: Args) -> anyhow::Result<()> {
 
     let mut store = Store::new(&engine, ());
 
-    let instance = linker.instantiate(&mut store, &component)?;
 
-    let greetable_index = instance
-        .get_export(&mut store, None, "axoloto210:greet/greetable")
-        .unwrap();
+    let provider = GreetableProvider::instantiate(&mut store, &component, &linker)?;
 
-    let greet_index = instance
-        .get_export(&mut store, Some(&greetable_index), "greet")
-        .unwrap();
+    let greetable = provider.axoloto210_greet_greetable();
 
-    let name_index = instance
-        .get_export(&mut store, Some(&greetable_index), "name")
-        .unwrap();
+    let message = greetable.call_greet(&mut store, "world")?;
 
-    let greet: TypedFunc<(String,), (String,)> = instance.get_typed_func(&mut store, greet_index).unwrap();
+    println!("{message}");
 
-    let name: TypedFunc<(), (String,)> = instance.get_typed_func(&mut store, name_index).unwrap();
+    let name = greetable.call_name(&mut store)?;
+    let message = greetable.call_greet(&mut store, &name)?;
+    println!("{message}");
 
-    let argument = "world!".to_string();
+    // let instance = linker.instantiate(&mut store, &component)?;
 
-    let (return_value,) = greet.call(&mut store, (argument,))?;
+    // let greetable_index = instance
+    //     .get_export(&mut store, None, "axoloto210:greet/greetable")
+    //     .unwrap();
 
-    greet.post_return(&mut store)?;
-    println!("{return_value}");
+    // let greet_index = instance
+    //     .get_export(&mut store, Some(&greetable_index), "greet")
+    //     .unwrap();
 
-    let (returned_name,) = name.call(&mut store, ())?;
-    name.post_return(&mut store)?;
+    // let name_index = instance
+    //     .get_export(&mut store, Some(&greetable_index), "name")
+    //     .unwrap();
 
-    let (return_value, ) = greet.call(&mut store, (returned_name,))?;
-    greet.post_return(&mut store)?;
+    // let greet: TypedFunc<(String,), (String,)> = instance.get_typed_func(&mut store, greet_index).unwrap();
 
-    println!("{return_value}");
+    // let name: TypedFunc<(), (String,)> = instance.get_typed_func(&mut store, name_index).unwrap();
+
+    // let argument = "world!".to_string();
+
+    // let (return_value,) = greet.call(&mut store, (argument,))?;
+
+    // greet.post_return(&mut store)?;
+    // println!("{return_value}");
+
+    // let (returned_name,) = name.call(&mut store, ())?;
+    // name.post_return(&mut store)?;
+
+    // let (return_value, ) = greet.call(&mut store, (returned_name,))?;
+    // greet.post_return(&mut store)?;
+
+    // println!("{return_value}");
 
     Ok({})
 }
