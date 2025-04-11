ac8da33a435698f424d1ed69a44de0283293b5f4
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-04-11 19:00:00 +0900

    wasip1でも動作するように。WamiViewの仕様が一部変わっていたので注意

--- dynamic-dependency-resolution-example/src/main.rs
+++ dynamic-dependency-resolution-example/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
+use wasmtime::component::{Component, Linker, ResourceTable, bindgen};
 use wasmtime::{Engine, Store};
-use wasmtime::component::{bindgen, Component, Linker};
-
+use wasmtime_wasi::{WasiCtx, WasiCtxBuilder, WasiView};
+use wasmtime_wasi::IoView;
 
 use axoloto210::greet::greetable::Host;
 
@@ -27,39 +28,70 @@ fn start(cli: Cli) -> anyhow::Result<()> {
     let engine = Engine::default();
     let mut linker: Linker<Greet> = Linker::new(&engine);
 
-    let mut store:Store<Greet> = Store::new(&engine, Greet::new("Native code".to_string()));
+    let mut store: Store<Greet> = Store::new(&engine, Greet::new("Native code".to_string()));
 
     let component = Component::from_file(&engine, &cli.wasm_file)?;
 
-
     HelloWorld::add_to_linker(&mut linker, |greet: &mut Greet| greet)?;
+
+    wasmtime_wasi::add_to_linker_sync(&mut linker)?;
 
     let hello_world = HelloWorld::instantiate(&mut store, &component, &linker)?;
 
-    let message = hello_world.axoloto210_greet_sayable().call_say(&mut store)?;
+    
+
+    let message = hello_world
+        .axoloto210_greet_sayable()
+        .call_say(&mut store)?;
     println!("{message}");
 
     Ok({})
 }
 
-
 struct Greet {
     name: String,
+    wasi_ctx: WasiCtx, // 環境変数のリストや標準入出力のストリーム、Wasmコンポーネントからのアクセスが許されているフォルダーのリストなどを保持。
+    resource_table: ResourceTable,
 }
 
 impl Greet {
-    fn new(name: String) -> Greet{
-        Greet {name}
+    fn new(name: String) -> Greet {
+
+        let wasi_ctx = WasiCtxBuilder::new().build();
+
+        let resource_table = ResourceTable::new();
+
+        Greet {
+            name,
+            wasi_ctx,
+            resource_table,
+        }
+    }
+}
+
+
+impl IoView for Greet {
+    fn table(&mut self) -> &mut ResourceTable {
+        &mut self.resource_table
+    }
+    
+
+}
+
+impl WasiView for Greet {
+
+    fn ctx (&mut self) -> &mut WasiCtx {
+        &mut self.wasi_ctx
     }
 }
 
 // Greetに自動生成されるHostトレイトを実装する。
 impl Host for Greet {
-    fn name(&mut self)->String{
+    fn name(&mut self) -> String {
         self.name.clone()
     }
 
-    fn greet(&mut self, name: String)->String{
+    fn greet(&mut self, name: String) -> String {
         format!("Hello from {name}")
     }
 }
